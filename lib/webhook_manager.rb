600788176d3c4d2cfb9719daf4b84743806c1c60
Luay Bseiso <luay@buttercloud.com>
2020-12-30 16:18:10 +0200

    downgrade Faraday + handle errors properly

--- lib/webhook_manager.rb
+++ lib/webhook_manager.rb
@@ -22,13 +22,25 @@ module WebhookManager
     end
 
     def trigger!(event_name:, payload:)
-      res = @conn.post("webhook_events/trigger", { event_name: event_name, event_payload: payload }.to_json)
+      begin
+        res = @conn.post("webhook_events/trigger", { event_name: event_name, event_payload: payload }.to_json)
+      rescue Faraday::ConnectionFailed => e
+        raise Error.new("Could not connect to API")
+      rescue Faraday::TimeoutError => e
+        raise Error.new("Time out error")
+      end
 
       handle_reponse(res)
     end
 
     def update_status(event_id:, status:)
-      res = @conn.post("webhook_events/update_status", { event_id: event_id, status: status }.to_json)
+      begin
+        res = @conn.post("webhook_events/update_status", { event_id: event_id, status: status }.to_json)
+      rescue Faraday::ConnectionFailed => e
+        raise Error.new("Could not connect to API")
+      rescue Faraday::TimeoutError => e
+        raise Error.new("Time out error")
+      end
 
       handle_reponse(res)
     end
@@ -39,11 +51,14 @@ module WebhookManager
       if res.status == 200
         data = JSON.parse(res.body)
 
-        if data["status"] == "ERROR"
+        case data["status"]
+        when "ERROR"
           raise "Something went wrong: #{data["errors"].join(", ")}"
-        elsif data["status"] == "OK"
+        when "OK"
           true
         end
+      elsif res.status == 401
+        raise Error.new("Unauthorized access to the API!")
       else
         raise Error.new("Something went wrong calling the hooky API!")
       end
--- webhook_manager.gemspec
+++ webhook_manager.gemspec
@@ -30,7 +30,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   # Uncomment to register a new dependency of your gem
-  spec.add_dependency "faraday", ">= 1.2.0"
+  spec.add_dependency "faraday", "~> 0.17.3"
   spec.add_development_dependency "pry"
 
   # For more information and examples about making a new gem, checkout our
